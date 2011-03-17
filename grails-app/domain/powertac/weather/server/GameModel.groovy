78aeb7862052182a9abfb6f10036d29107d334da
eonarheim <onarh001@umn.edu>
2011-03-17 12:17:14 -0500

    Basic logical framework laid out

---
+++ grails-app/domain/powertac/weather/server/GameModel.groovy
@@ -0,0 +1,13 @@
+package powertac.weather.server
+
+class GameModel {
+	int gameId
+	Date syncStart
+	int currentDay
+	int currentTime
+	int duration
+	boolean isActive
+	
+    static constraints = {
+    }
+}
--- grails-app/domain/powertac/weather/server/WeatherSet.groovy
+++ grails-app/domain/powertac/weather/server/WeatherSet.groovy
@@ -4,11 +4,38 @@ package powertac.weather.server
 class WeatherSet {
 	Date startDate
 	int numberDays
-	int currentDay
-	int currentTime
 	boolean fetched
-	WeatherReport set = []
+	//WeatherReport set = []
 	
     static constraints = {
-    }	
+    }
+	
+	static hasMany = [reports:WeatherReport,games:GameModel]
+	
+	/*
+	 * returns the applicable weatherReport for the active GameModel ID
+	 */
+	
+	def gen48Forecast(int gameId){
+		return new WeatherReport()
+	}
+	
+	
+	/*
+	 * Updates the current day and time with all active game models.
+	 * Deletes inactive models from the database.
+	 */
+	def update()  {
+		
+	}
+	
+	/*
+	 * Generates the initial weather reports for the database.
+	 * This should only need to be called once.
+	 */
+	def genReports() {
+		if(!fetched){
+			// Retrieve info from database and create WeatherReports
+		}
+	}
 }
