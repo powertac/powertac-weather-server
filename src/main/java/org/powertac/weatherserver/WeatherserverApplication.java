02d083ffeda73506f2ddf8c976d4e8b7862176b1
Frederik Milkau <fmilkau@uni-koeln.de>
2021-10-21 15:45:54 +0200

    boilerplate init

---
+++ src/main/java/org/powertac/weatherserver/WeatherserverApplication.java
@@ -0,0 +1,13 @@
+package org.powertac.weatherserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/powertac/weatherserver/WeatherserverApplicationTests.java
@@ -0,0 +1,13 @@
+package org.powertac.weatherserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
